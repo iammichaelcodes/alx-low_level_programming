c9fcab1aec8ed941c1efe46b7be41972831eded0
Michael Codes <michaelcodes52@gmail.com>
2022-09-09 20:26:35 -0700

    updated print.c

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main- print the string in the main function
+ *
+ * Description: using the main function
+ *
+ * this program prints "programming is building a multilingual puzzle
+ *
+ * Return: 0
+ */
+ int main(void) 
+ {
+	 printf("with proper grammer,but the outcome is a piece of art,\n");
+	 
+	 return(0);
+ }
