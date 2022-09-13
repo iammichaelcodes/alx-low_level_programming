46786c34a89c37d6013ccdefa95a493a0314bcb9
iammichaelcodes <mikeevboumwan52@gmail.com>
2022-09-13 18:32:12 +0300

    project updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,12 @@
 #include "main.h"
 
-
-
 /**
  * main - print putchar
  * Return: always 0
  */
 
-
-
 int main(void)
-
 {	
-
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -23,10 +17,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-
-
 	return (0);
-
-
-
 }
