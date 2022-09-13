df4904cf731200f20785875c255f143d31f72d91
Michael Codes <michaelcodes52@gmail.com>
2022-09-13 06:55:34 -0700

    putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,31 @@
-#include <stdio.h>
+#include "main.h"
 
-int main(void) 
-{		
-	char ch = '_putchar' ;
-	while(ch)
-	{
-		putchar(ch);
-		ch++;
-	}
-	putchar('\n');
-	return(0);
+int main(void)
+
+{
+
+	_putchar('_');
+
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
+	_putchar('\n');
+
+
+
+	return (0);
+
+
+
 }
