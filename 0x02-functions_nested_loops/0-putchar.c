b9da968174fb0d10629d09951745e7eb6da5589c
Michael Codes <michaelcodes52@gmail.com>
2022-09-14 09:18:47 -0700

    11-print_to_98

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,5 @@
+
 #include "main.h"
-
-
 
 /**
 
@@ -8,36 +7,3 @@
 
  * Return: always 0
 
- */
-
-
-
-int main(void)
-
-{
-
-	_putchar('_');
-
-	_putchar('p');
-
-	_putchar('u');
-
-	_putchar('t');
-
-	_putchar('c');
-
-	_putchar('h');
-
-	_putchar('a');
-
-	_putchar('r');
-
-	_putchar('\n');
-
-
-
-	return (0);
-
-
-
-}
